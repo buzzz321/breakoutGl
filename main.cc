d69afe0c8b246d853bf430b43f7c4fc913537eb9
Anders Olme <anders.olme@gmail.com>
2020-12-30 09:57:11 +0100

    updates from windows
    Add possibillity to build from windows.

--- main.cc
+++ main.cc
@@ -167,6 +167,12 @@ int main() {
 
   glfwSetErrorCallback(error_callback);
 
+    glfwSetErrorCallback(error_callback);
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
   GLFWwindow *window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "My Title",
                                         nullptr, nullptr);
   if (!window) {
@@ -185,6 +191,14 @@ int main() {
     glfwTerminate();
     exit(1);
   }
+
+   glEnable(GL_DEBUG_OUTPUT);
+   glDebugMessageCallback(
+        [](GLenum source, GLenum type, GLuint id, GLenum severity,
+           GLsizei length, const GLchar* message, const void* userParam) {
+            std::cout << "OpenGL Error/Warning: " << message << std::endl;
+        },
+        nullptr);
 
   // Enable depth test
   glEnable(GL_DEPTH_TEST);
